b6674947fc506789e981d3ef3ff364a6934ebf0a
Danmedflo <danmedflo98@gmail.com>
2026-06-25 19:17:27 -0500

    Primeras configuraciones de rutas

--- frontend-coreflow/src/app/app.routes.ts
+++ frontend-coreflow/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Productos } from './pages/productos/productos';
+import { ProductoForm } from './pages/producto-form/producto-form';
+import { Pedidos } from './pages/pedidos/pedidos';
+import { PedidoForm } from './pages/pedido-form/pedido-form';
+import { NotFound } from './pages/not-found/not-found';
+
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Inicio' },
+
+  { path: 'login', component: Login, title: 'Login' },
+  { path: 'registro', component: Register, title: 'Registro' },
+
+  { path: 'productos', component: Productos, title: 'Productos' },
+  { path: 'productos/nuevo', component: ProductoForm, title: 'Nuevo producto' },
+  { path: 'productos/editar/:id', component: ProductoForm, title: 'Editar producto' },
+
+  { path: 'pedidos', component: Pedidos, title: 'Pedidos' },
+  { path: 'pedidos/nuevo', component: PedidoForm, title: 'Nuevo pedido' },
+  { path: 'pedidos/editar/:id', component: PedidoForm, title: 'Editar pedido' },
+
+  { path: '**', component: NotFound, title: 'Página no encontrada' }
+];
