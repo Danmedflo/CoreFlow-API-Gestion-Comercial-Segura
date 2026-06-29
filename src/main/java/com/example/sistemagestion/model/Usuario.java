ca7665ac36a30d86f5058304aeb096b155f4e672
Danmedflo <danmedflo98@gmail.com>
2026-06-28 23:20:23 -0500

    Actualizaciones de perfil

--- src/main/java/com/example/sistemagestion/model/Usuario.java
+++ src/main/java/com/example/sistemagestion/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.example.sistemagestion.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +15,9 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
+
     private String password;
     private String rol;
     private boolean activo;
